99b3e0d5564a0cb337b2e749c89c1821c2af3711
Pranavi Baraskar <141246026+PranaviBaraskar@users.noreply.github.com>
2023-08-02 19:44:39 +0530

    Add files via upload

--- program4.c
+++ program4.c
@@ -1,32 +1,7 @@
-/*
-    Steps to create the application
-
-    Step 1 : Understand the problem statement
-    Step 2 : Write the algorithm
-    Step 3 : Decide the programming language
-    Step 4 : Write the program
-    Step 5 : Test the written program 
-
-*/
-
-// Wriete a program which performs addition of two numbers
-
-/*
-    Algorithm
-
-    START
-        Accept First Number as No1
-        Accept Second Number as No2
-        Create one variable as ANS
-        Perform Addition of No1 and No2
-        Store the addition into variable ANS
-        Display the value of ANS
-    STOP
-*/
 
 /////////////////////////////////////////////////////////////////////////////////
 //
-// Problem Statement : Wriete a program which performs addition of two numbers
+// Problem Statement : Write a program which performs addition of two numbers
 //
 /////////////////////////////////////////////////////////////////////////////////
 
@@ -38,8 +13,7 @@
 //  Input :         Unsigned integer, Unsigned integer
 //  Output :        Unsigned integer
 //  Description :   Performs addition of 2 numbers
-//  Author :        Piyush Manohar Khairnar
-//  Date :          18/04/2023
+//  Author :        Pranavi Ghanshyam Baraskar
 //
 /////////////////////////////////////////////////////////////////////////////////
 
